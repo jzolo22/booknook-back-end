7cceb0061b818d823ac709909596db45265d215b
jzolo22 <jzolotarev@gmail.com>
2020-12-01 18:56:49 -0500

    removed duplicates from book array

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,4 +5,14 @@ class BooksController < ApplicationController
         render json: book
     end
 
+    def index
+        books = Book.all
+        shuffled_books = books.uniq do |book|
+            [
+                book[:title]
+            ]
+        end.shuffle
+        render json: shuffled_books
+    end
+
 end
--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,5 +1,5 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes  :title, :author, :id, :image_url
+  attributes  :title, :author, :id, :image_url, :description
   has_many :reviews
   # make sure syntax is correct
 end
