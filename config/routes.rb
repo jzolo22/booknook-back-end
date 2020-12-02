b05bb970c631f239a3054963aa36f4a713172cc6
jzolo22 <jzolotarev@gmail.com>
2020-12-02 14:58:00 -0500

    added routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:show, :index, :create, :update]
+  resources :users, only: [:create]
+  resources :reviews, only: [:create, :update, :index]
 end
